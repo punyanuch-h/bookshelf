3f0ed511ceecab751539cf5a62dc76a17c7d3a1d
punyanuch-h <punyanuch.hat@student.mahidol.edu>
2024-10-25 18:52:25 +0700

    Initial commit for bookshelf project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// src/Home.jsx
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to the Bookshelf</h1>
+      <p>Explore and organize your favorite books.</p>
+    </div>
+  );
+};
+
+export default Home;
